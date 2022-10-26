b450a1fcdd6c84a1dc53a1806a66a5b802003712
Yousef Alhujaylan <yousef.alhujaylan@kcl.ac.uk>
2022-10-26 20:18:17 +0100

    Fix bug (add max value)

--- things/models.py
+++ things/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Thing(models.Model):
-    name = models.CharField()
-    description = models.CharField()
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=120)
     quantity = models.IntegerField()
