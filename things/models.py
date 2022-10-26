b3b91366f3b3617775c2b31830c52ea807d8190c
Yousef Alhujaylan <yousef.alhujaylan@kcl.ac.uk>
2022-10-26 20:29:48 +0100

    Add validators and constraints to fields.

--- things/models.py
+++ things/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
 class Thing(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=120)
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, blank=False, unique=True)
+    description = models.CharField(max_length=120, blank=True, unique=False)
+    quantity = models.IntegerField(
+    blank=False,
+    validators =[
+        MaxValueValidator(100),
+        MinValueValidator(1)
+    ])
