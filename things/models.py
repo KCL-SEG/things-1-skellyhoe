9a3f3861c8ce6e0b20915019aab625b034fb77fd
Yousef Alhujaylan <yousef.alhujaylan@kcl.ac.uk>
2022-10-27 18:24:57 +0100

    Try to fix constraint bug.

--- things/models.py
+++ things/models.py
@@ -4,12 +4,20 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class Thing(models.Model):
-    name = models.CharField(max_length=30, blank=False, unique=True)
-    description = models.CharField(max_length=120, blank=True, unique=False)
+    name = models.CharField(max_length=30,
+     unique=True,
+     blank=False
+     )
+    description = models.CharField(max_length=120,
+    unique = False,
+    blank=True
+    )
     quantity = models.IntegerField(
-    unique=False,
+    unique = False,
+    default=1,
     validators=[
         MinValueValidator(limit_value=0),
         MaxValueValidator(limit_value=100),
 
-    ])
+    ]
+    )
