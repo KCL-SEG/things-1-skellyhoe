c4850ee352ea035aee0c2cde561f5cb5de02500b
Yousef Alhujaylan <yousef.alhujaylan@kcl.ac.uk>
2022-10-27 17:47:49 +0100

    Try to fix constraint bug.

--- things/models.py
+++ things/models.py
@@ -7,8 +7,9 @@ class Thing(models.Model):
     name = models.CharField(max_length=30, blank=False, unique=True)
     description = models.CharField(max_length=120, blank=True, unique=False)
     quantity = models.IntegerField(
-    blank=False,
+    unique=False,
     validators=[
+        MinValueValidator(limit_value=0),
         MaxValueValidator(limit_value=100),
-        MinValueValidator(limit_value=0)
+
     ])
